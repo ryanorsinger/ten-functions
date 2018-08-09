076852b5de6d917b604e4a7646992f96a4542156
Justin Luoma <justin@justinluoma.com>
2018-08-09 14:44:59 -0500

    Removed solutions-edit-like related code from client.js

--- client.js
+++ client.js
@@ -8,11 +8,6 @@
 //   // ipAddress
 // });
 
-// projectName and projectEditLink
-let projectName = location.host.substring(0, location.host.indexOf("."));
-let editSolutionsUrl = "https://glitch.com/edit/#!/"+ projectName + "?path=solutions.js";       
-document.getElementById("solutions-edit-link").href = editSolutionsUrl;
-
 // Add tracker to count number of remixes/forks of the project
 document.querySelector("#remix").addEventListener('click', function(event) {
   console.log("remix button clicked");
